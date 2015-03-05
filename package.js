e9ca789ed5bbef14c2373f3da72a2345eb710328
Rob Fallows <rfallows@centiq.co.uk>
2015-03-05 17:19:29 +0000

    added missing images

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'tunguska:gauge-themes',
-  version: '1.0.1',
+  version: '1.0.2',
   summary: 'A small theme pack for tunguska:gauge',
   git: 'https://github.com/robfallows/tunguska-gauge-themes',
   documentation: 'README.md'
@@ -9,6 +9,16 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.0.3.1');
   api.addFiles('tunguska:gauge-themes.js', 'client');
+  api.addFiles([
+    'images/steampunk01.png',
+    'images/steampunk02.png',
+    'images/steampunk03.png',
+    'images/steampunk04.png',
+    'images/steampunk05.png',
+    'images/rotarybg.png',
+    'images/rotaryfg.png',
+    'images/rotaryp.png'
+  ], 'client');
   api.export('TunguskaGaugeThemePack', 'client');
 });
 
